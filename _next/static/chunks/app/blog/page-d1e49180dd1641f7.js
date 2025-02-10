42d38c3dbf4cbfea01bb7695152775fc8cb86832
Master Chief <bassamkhanpakistan99@gmail.com>
2025-02-10 23:57:42 +0500

    Updates

---
+++ _next/static/chunks/app/blog/page-d1e49180dd1641f7.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[404],{4354:function(e,t,r){Promise.resolve().then(r.bind(r,9978))},9978:function(e,t,r){"use strict";r.r(t),r.d(t,{default:function(){return BlogPage}});var n=r(5629),o=r(6868),s=r(8849);function BlogPage(){let e=(0,s.useRouter)();return(0,o.useEffect)(()=>{e.push("https://blog.snowdensvault.fyi/")},[e]),(0,n.jsx)("div",{className:"flex items-center justify-center min-h-screen bg-zinc-900 text-white",children:(0,n.jsx)("p",{children:"Redirecting to blog..."})})}},930:function(e,t,r){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=r(6868),o=Symbol.for("react.element"),s=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),u=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,i={key:!0,ref:!0,__self:!0,__source:!0};function q(e,t,r){var n,f={},c=null,l=null;for(n in void 0!==r&&(c=""+r),void 0!==t.key&&(c=""+t.key),void 0!==t.ref&&(l=t.ref),t)s.call(t,n)&&!i.hasOwnProperty(n)&&(f[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===f[n]&&(f[n]=t[n]);return{$$typeof:o,type:e,key:c,ref:l,props:f,_owner:u.current}}t.jsx=q,t.jsxs=q},5629:function(e,t,r){"use strict";e.exports=r(930)},8849:function(e,t,r){e.exports=r(9116)}},function(e){e.O(0,[259,57,744],function(){return e(e.s=4354)}),_N_E=e.O()}]);
---
+++ _next/static/chunks/app/portfolio/page-53487a40166b4b89.js
@@ -0,0 +1,9 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[444],{1034:function(e,t,r){Promise.resolve().then(r.bind(r,1960))},1960:function(e,t,r){"use strict";r.r(t),r.d(t,{default:function(){return PortfolioPage}});var n=r(5629),o=r(6868),i=r(8849);function PortfolioPage(){let e=(0,i.useRouter)();return(0,o.useEffect)(()=>{e.push("https://terminalcv.snowdensvault.fyi/")},[e]),(0,n.jsx)("div",{className:"flex items-center justify-center min-h-screen bg-zinc-900 text-white",children:(0,n.jsx)("p",{children:"Redirecting to portfolio..."})})}},930:function(e,t,r){"use strict";/**
+ * @license React
+ * react-jsx-runtime.production.min.js
+ *
+ * Copyright (c) Meta Platforms, Inc. and affiliates.
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */var n=r(6868),o=Symbol.for("react.element"),i=(Symbol.for("react.fragment"),Object.prototype.hasOwnProperty),s=n.__SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED.ReactCurrentOwner,f={key:!0,ref:!0,__self:!0,__source:!0};function q(e,t,r){var n,u={},c=null,l=null;for(n in void 0!==r&&(c=""+r),void 0!==t.key&&(c=""+t.key),void 0!==t.ref&&(l=t.ref),t)i.call(t,n)&&!f.hasOwnProperty(n)&&(u[n]=t[n]);if(e&&e.defaultProps)for(n in t=e.defaultProps)void 0===u[n]&&(u[n]=t[n]);return{$$typeof:o,type:e,key:c,ref:l,props:u,_owner:s.current}}t.jsx=q,t.jsxs=q},5629:function(e,t,r){"use strict";e.exports=r(930)},8849:function(e,t,r){e.exports=r(9116)}},function(e){e.O(0,[259,57,744],function(){return e(e.s=1034)}),_N_E=e.O()}]);
